62a6b06e6f8450cf3d36eb3d00e8af33e5f7ff8a
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2021-06-14 19:11:41 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -2,10 +2,17 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
+import numpy
+import tflearn
+import tensorflow
+import random
 
 import json
 
 words=[]
+docs_x=[]
+docs_y=[]
+labels=[]
 
 with open('intents.json') as file:
     data=json.load(file)
@@ -13,7 +20,58 @@ for intent in data['intents']:
     for pattern in intent['patterns']:
         wrds=word_tokenize(pattern)
         words.extend(wrds)
-        #print(word_tokenize(pattern))
-words=[stemmer.stem(w.lower()) for w in words]
-words=sorted(list(set(words)))
-print(words)
+        docs_x.append(wrds)
+        docs_y.append(intent['tag'])
+        
+    if intent['tag'] not in labels:
+        labels.append(intent['tag'])
+        
+        
+words = [stemmer.stem(w.lower()) for w in words if w !='?']
+words = sorted(list(set(words)))
+
+labels = sorted(labels)
+
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(labels))]
+
+for x, doc in enumerate(docs_x):
+    bag=[]
+
+    wrds = [stemmer.stem(w.lower()) for w in doc]
+
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
+    training.append(bag)
+    output.append(output_row)
+
+
+training = numpy.array(training)
+output = numpy.array(output)
+
+# now we create the neural networks
+
+tensorflow.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation = 'softmax')
+net = tflearn.regression(net)
+     
+model = tflearn.DNN(net)
+
+model.fit(training, output, n_epoch=1000, batch_size=8, show_metric = True)
+model.save('model.tflearn')
+
+            
+            
