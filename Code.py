85c8b8c5d7bd4b81d07762b8761d8481f9bdf0bf
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2021-12-19 15:22:15 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,3 +1,4 @@
+import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
@@ -73,5 +74,40 @@ model = tflearn.DNN(net)
 model.fit(training, output, n_epoch=1000, batch_size=8, show_metric = True)
 model.save('model.tflearn')
 
+
+#new code
+
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
             
+    return numpy.array(bag)
+
+
+def chat():
+    print("Start talking with the bot (type quit to stop)!")
+    while True:
+        inp = input("You: ")
+        if inp.lower() == "quit":
+            break
+
+        results = model.predict([bag_of_words(inp, words)])
+        results_index = numpy.argmax(results)
+        tag = labels[results_index]
+
+        for tg in data["intents"]:
+            if tg['tag'] == tag:
+                responses = tg['responses']
+
+        print(random.choice(responses))
+
+chat()
             
+     
